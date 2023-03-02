e8a43d6cf046a0fdcb577338da7eb2e7fe5250c7
Matsui_Kennosuke <k.matsui@lull-inc.co.jp>
2023-03-02 18:38:58 +0900

    reset-App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Hello React App with AWS</h2>
-      </header>
     </div>
   );
 }
