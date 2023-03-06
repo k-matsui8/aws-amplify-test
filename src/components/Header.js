55302c3c3e24aafeeab6b287df18e047a6d48ce7
Matsui_Kennosuke <k.matsui@lull-inc.co.jp>
2023-03-06 09:45:57 +0900

    header

---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header id="masthead" class="header">
+      <div class="head-inner">
+        <div class="head-sitename">
+          <a href="/">
+            <div class="head-sitename_title">
+              <div class="js-splited" >r</div>
+              <div class="js-splited" >e</div>
+              <div class="js-splited" >c</div>
+              <div class="js-splited" >r</div>
+              <div class="js-splited" >u</div>
+              <div class="js-splited" >i</div>
+              <div class="js-splited" >t</div>
+            </div>
+          </a>
+        </div>
+        <nav class="site-menu">
+          <ul class="site-menu-list">
+            <li class="site-menu-item">
+              <a class="site-menu-item_link" href="/news/">news</a>
+            </li>
+            <li class="site-menu-item">
+              <a class="site-menu-item_link" href="/#service">service</a>
+            </li>
+            <li class="site-menu-item">
+              <a class="site-menu-item_link" href="/interview/">interview</a>
+            </li>
+            <li class="site-menu-item">
+              <a class="site-menu-item_link" href="/position/">position</a>
+            </li>
+            <li class="site-menu-item site-menu-item--button">
+              <a class="entry-button" href="/entry/">entry</a>
+            </li>
+          </ul>
+        </nav>
+
+      </div>
+    </header>
+  )
+}
+
+export default Header
